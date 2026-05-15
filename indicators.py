8a4caa587aaaf936f140c6c7fcb5d34c148679d6
hlole638-lang <hlole638@gmail.com>
2026-05-15 21:46:59 +0300

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,1 +1,11 @@
+import pandas_ta as ta
 
+def add_rsi(df):
+    df["RSI"] = ta.rsi(df["close"], length=14)
+    return df
+
+def candle_body(df):
+    return abs(df["close"] - df["open"])
+
+def avg_body(df):
+    return candle_body(df).rolling(3).mean()
