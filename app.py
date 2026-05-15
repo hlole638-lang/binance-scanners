a0a48382c3dedfeb258c778b53cf21f558e36186
hlole638-lang <hlole638@gmail.com>
2026-05-16 00:13:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
 @app.route("/scan/<timeframe>")
@@ -15,4 +16,5 @@ def scan(timeframe):
     return jsonify(data)
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=3000)
