f9224d22b0c76ac77cfab53dc8759ed4c615788c
hlole638-lang <hlole638@gmail.com>
2026-05-15 22:21:41 +0300

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,7 +1,19 @@
-import pandas_ta as ta
+import pandas as pd
 
-def add_rsi(df):
-    df["RSI"] = ta.rsi(df["close"], length=14)
+def add_rsi(df, period=14):
+
+    delta = df["close"].diff()
+
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.rolling(period).mean()
+    avg_loss = loss.rolling(period).mean()
+
+    rs = avg_gain / avg_loss
+
+    df["RSI"] = 100 - (100 / (1 + rs))
+
     return df
 
 def candle_body(df):
